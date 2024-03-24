115ef84e7d88d83886f4ebccad9fbdf31870531a
Aavash Paudel <muproject990@gmail.com>
2024-03-25 03:23:57 +0545

    ui setup

---
+++ lib/features/auth/presentation/pages/login-page.dart
@@ -0,0 +1,95 @@
+import 'package:blog_app/core/theme/app_pallete.dart';
+import 'package:blog_app/features/auth/presentation/widgets/auth-gradient-button.dart';
+import 'package:blog_app/features/auth/presentation/widgets/auth_field.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  //! Now using TextEditiong controller For accesing value in the text Box
+
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  // final nameController = TextEditingController();
+
+  final formKey = GlobalKey<FormState>();
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    // nameController.dispose();
+    super.dispose();
+  }
+
+  Widget build(BuildContext context) {
+    // it will validate every form feild
+
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            // crossAxisAlignment: Cr,
+            children: [
+              const Text(
+                "Sign In",
+                style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              // AuthField(
+              //   hintText: "Name",
+              //   controller: nameController,
+              // ),
+              // const SizedBox(
+              //   height: 15,
+              // ),
+              AuthField(
+                hintText: "Email",
+                controller: emailController,
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              AuthField(
+                isObscureText: true,
+                hintText: "Password",
+                controller: passwordController,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const AuthGradientBtn(),
+              const SizedBox(
+                height: 20,
+              ),
+              RichText(
+                text: TextSpan(
+                  text: "Don\'t have an account ? ",
+                  style: Theme.of(context).textTheme.titleMedium,
+                  children: [
+                    TextSpan(
+                      text: "Sign In",
+                      style: Theme.of(context)
+                          .textTheme
+                          .titleMedium
+                          ?.copyWith(color: AppPallete.gradient2),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
