473dc4ce6733761decc4d9d958174263874bacc8
Aavash Paudel <muproject990@gmail.com>
2024-03-25 14:21:02 +0545

    done

---
+++ lib/core/secrets/app-secret.dart
@@ -0,0 +1,5 @@
+class AppSecrets {
+  static const supabaseUrl = 'https://sdbewnfjnfnrhouyihwl.supabase.co';
+  static const supabaseAnnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNkYmV3bmZqbmZucmhvdXlpaHdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTEzNTQwNzQsImV4cCI6MjAyNjkzMDA3NH0.0ycuiR8Rv4jzXt-MQtS6FzQXKTdkSz7jBh_MtA1nCVg';
+}
