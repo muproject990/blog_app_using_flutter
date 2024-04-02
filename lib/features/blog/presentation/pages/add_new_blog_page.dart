ccaf27db19d4e1d7d781d41d2e7e5b1aeebe1f11
Aavash Paudel <muproject990@gmail.com>
2024-04-02 13:12:05 +0545

    4.58.41 after that uplading image to databases

--- lib/features/blog/presentation/pages/add_new_blog_page.dart
+++ lib/features/blog/presentation/pages/add_new_blog_page.dart
@@ -1,9 +1,11 @@
-import 'dart:ffi';
+import 'dart:io';
 
 import 'package:blog_app/core/theme/app_pallete.dart';
+import 'package:blog_app/core/utils/pick_image.dart';
 import 'package:blog_app/features/blog/presentation/widgets/blog_editor.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class AddNewBlogPage extends StatefulWidget {
   static route() =>
@@ -20,6 +22,17 @@ class _AddNewBlogPageState extends State<AddNewBlogPage> {
   final titleController = TextEditingController();
   final contentController = TextEditingController();
   List<String> selectedTopics = [];
+  File? image;
+
+  void selectImage() async {
+    final pickedImage = await pickImage();
+
+    if (pickedImage != null) {
+      setState(() {
+        image = pickedImage;
+      });
+    }
+  }
 
   @override
   void dispose() {
@@ -44,23 +57,45 @@ class _AddNewBlogPageState extends State<AddNewBlogPage> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            DottedBorder(
-                color: AppPallete.borderColor,
-                dashPattern: const [10, 4],
-                radius: const Radius.circular(10),
-                strokeCap: StrokeCap.round,
-                child: Container(
-                  height: 150,
-                  width: double.infinity,
-                  child: const Column(
-                    children: [
-                      Icon(
-                        Icons.folder_open,
-                        size: 40,
+            image != null
+                ? GestureDetector(
+                    onTap: () {
+                      selectImage();
+                    },
+                    child: SizedBox(
+                      width: double.infinity,
+                      height: 150,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.file(
+                          image!,
+                          fit: BoxFit.cover,
+                        ),
                       ),
-                    ],
+                    ),
+                  )
+                : GestureDetector(
+                    onTap: () {
+                      selectImage();
+                    },
+                    child: DottedBorder(
+                        color: AppPallete.borderColor,
+                        dashPattern: const [10, 4],
+                        radius: const Radius.circular(10),
+                        strokeCap: StrokeCap.round,
+                        child: Container(
+                          height: 150,
+                          width: double.infinity,
+                          child: const Column(
+                            children: [
+                              Icon(
+                                Icons.folder_open,
+                                size: 40,
+                              ),
+                            ],
+                          ),
+                        )),
                   ),
-                )),
             const SizedBox(
               height: 10,
             ),
@@ -89,7 +124,7 @@ class _AddNewBlogPageState extends State<AddNewBlogPage> {
                           },
                           child: Chip(
                             color: selectedTopics.contains(e)
-                                ? MaterialStatePropertyAll(
+                                ? const MaterialStatePropertyAll(
                                     AppPallete.gradient1,
                                   )
                                 : null,
