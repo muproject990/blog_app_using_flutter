372587f95aa12b305cef882f98819ea4f0a33e73
Aavash Paudel <muproject990@gmail.com>
2024-04-03 03:46:36 +0545

    worked completed and show title on the home page

--- lib/features/blog/presentation/pages/blog_page.dart
+++ lib/features/blog/presentation/pages/blog_page.dart
@@ -1,12 +1,27 @@
+import 'package:blog_app/core/common/widgets/loader.dart';
+import 'package:blog_app/core/utils/show-snackbar.dart';
+import 'package:blog_app/features/blog/presentation/bloc/blog_bloc.dart';
 import 'package:blog_app/features/blog/presentation/pages/add_new_blog_page.dart';
 import 'package:flutter/cupertino.dart';
 // import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 // import 'package:flutter/widgets.dart';
 
-class BlogPage extends StatelessWidget {
+class BlogPage extends StatefulWidget {
   static route() => MaterialPageRoute(builder: (context) => const BlogPage());
   const BlogPage({super.key});
+
+  @override
+  State<BlogPage> createState() => _BlogPageState();
+}
+
+class _BlogPageState extends State<BlogPage> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<BlogBloc>().add(BlogFetchAllBlogs());
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,6 +38,28 @@ class BlogPage extends StatelessWidget {
             ),
           ),
         ],
+      ),
+      body: BlocConsumer<BlogBloc, BlogState>(
+        listener: (context, state) {
+          if (state is BlogFailure) {
+            showSnackBar(context, state.error);
+          }
+        },
+        builder: (context, state) {
+          if (state is BlogLoading) {
+            return const Loader();
+          }
+
+          if (state is BlogsDisplaySuccess) {
+            return ListView.builder(
+                itemCount: state.blogs.length,
+                itemBuilder: (context, index) {
+                  final blog = state.blogs[index];
+                  return Text(blog.title);
+                });
+          }
+          return const SizedBox();
+        },
       ),
     );
   }
