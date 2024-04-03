ed170692eae099cea287fbd90ef2856582719379
Aavash Paudel <muproject990@gmail.com>
2024-04-03 16:03:14 +0545

    6.23

--- lib/features/blog/presentation/pages/blog_viewer_page.dart
+++ lib/features/blog/presentation/pages/blog_viewer_page.dart
@@ -1,17 +1,74 @@
+import 'package:blog_app/core/theme/app_pallete.dart';
+import 'package:blog_app/core/utils/calculate_reading_time.dart';
+import 'package:blog_app/features/blog/domain/entities/blog.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class BlogViewerPage extends StatelessWidget {
-  static route() => MaterialPageRoute(
-        builder: (context) => const BlogViewerPage(),
+  static route(Blog blog) => MaterialPageRoute(
+        builder: (context) => BlogViewerPage(
+          blog: blog,
+        ),
       );
-  const BlogViewerPage({super.key});
+  final Blog blog;
+  const BlogViewerPage({super.key, required this.blog});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                blog.title,
+                style: const TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                "By ${blog.posterName}",
+                style: const TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Text(
+                '${blog.updatedAt} . ${calculateReadingTime(blog.content)}',
+                style: const TextStyle(
+                  fontWeight: FontWeight.w500,
+                  color: AppPallete.greyColor,
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(height: 20),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.network(blog.imageUrl),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                blog.content,
+                style: const TextStyle(
+                  fontSize: 16,
+                  height: 2,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/features/blog/presentation/widgets/blog_card.dart
+++ lib/features/blog/presentation/widgets/blog_card.dart
@@ -13,7 +13,7 @@ class BlogCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        Navigator.push(context, BlogViewerPage.route());
+        Navigator.push(context, BlogViewerPage.route(blog));
       },
       child: Container(
         margin: const EdgeInsets.all(16).copyWith(bottom: 4),
